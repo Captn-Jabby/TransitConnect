3136faeff31727e106779a014a83fd206acac012
Monishrajpalanivelu <142282793+Monishrajpalanivelu@users.noreply.github.com>
2026-02-01 13:04:17 +0530

    Update AuthController.java

--- backend/src/main/java/com/connect/transitconnect/controller/AuthController.java
+++ backend/src/main/java/com/connect/transitconnect/controller/AuthController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin("*")
+// @CrossOrigin("*")
 public class AuthController {
 
       private final AuthService authService;
